e805a29cb5a52bb2add27a9d48b5fb4d4983e03d
Elias0902 <eliasarmas0902@gmail.com>
2025-06-29 21:49:22 -0400

    Sistema Natys arriba en la pestaña

--- Assets/layouts/base.php
+++ Assets/layouts/base.php
@@ -3,7 +3,7 @@
 <html lang="es">
 <head>
     <meta charset="UTF-8">
-    <title><?= $title ?? 'Natys System' ?></title>
+    <title><?= $title ?? 'SISTEMA DE GALLETAS NATYS' ?></title>
     <link rel="icon" href="../Natys/Assets/img/natys.png" type="image/x-icon">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
